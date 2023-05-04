0ed58fe377ccc5f245a1924f9af5c8b490067c2d
michelle-viljoen <126607925+michelle-viljoen@users.noreply.github.com>
2023-05-04 16:40:25 +0200

    Add files via upload
    Challenge 1 complete (IWA6)

---
+++ Challenge1/script.js
@@ -0,0 +1,12 @@
+const primaryPhone = 'O748105141'
+const secondaryPhone = '0219131568'
+
+// Only change below this line
+
+
+const primaryValid = (parseInt(primaryPhone) ? 'true' : 'false')
+const secondaryValid = (parseInt(secondaryPhone) ? 'true' : 'false')
+
+
+console.log('Primary phone is valid numerical string:', primaryValid)
+console.log('Secondary phone is valid numerical string:', secondaryValid )
